7f265d8ad07d4e58307f62a2f2db0ae79cfb7e1c
Gekif <dzulfikar.maulana@gmail.com>
2019-09-29 14:15:34 +0700

    17: HTML Form For Adding A Category

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.giflib.web.controller;
 
 import com.teamtreehouse.giflib.model.Category;
 import com.teamtreehouse.giflib.service.CategoryService;
+import com.teamtreehouse.giflib.web.Color;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,8 @@ public class CategoryController {
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
         // TODO: Add model attributes needed for new form
+        model.addAttribute("category", new Category());
+        model.addAttribute("colors", Color.values());
 
         return "category/form";
     }
